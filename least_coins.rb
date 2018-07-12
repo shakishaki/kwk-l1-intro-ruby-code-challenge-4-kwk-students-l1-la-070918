afce0512645bdd925f008d77787535c10d919865
shakishaki <shaki.canchola@yahoo.com>
2018-07-12 16:37:50 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -17,8 +17,8 @@ coins = {
 end
 def user_input
   puts "What is your coin value?"
-  number_1=gets.chomp
-  cents=number_1.to_f*100
+  number_1 = gets.chomp
+  cents = number_1.to_f*100
   puts "#{cents}"
  if number_1.to_f > 1
   puts "Enter a value that is less than 1."
