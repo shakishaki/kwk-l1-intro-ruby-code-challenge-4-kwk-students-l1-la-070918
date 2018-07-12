27985bcdaaa9d3a599a7de2a265c82aa65a8cfc2
shakishaki <shaki.canchola@yahoo.com>
2018-07-12 16:37:07 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,33 @@
-#write out your code here
+# #write out your code here
 
-def least_coins(cents)
+# def least_coins(cents)
 
-#Code your answer here!
+# #Code your answer here!
 
+# end
+
+def coin_1
+
+coins = { 
+  "quarters"=>[1, 2, 3, 4],
+  "dimes"=>[1, 2, 3, 4],
+  "nickels"=>[1, 2, 3, 4],
+  "pennies"=> [1, 2, 3 , 4]
+}
+end
+def user_input
+  puts "What is your coin value?"
+  number_1=gets.chomp
+  cents=number_1.to_f*100
+  puts "#{cents}"
+ if number_1.to_f > 1
+  puts "Enter a value that is less than 1."
+  end
+end
+puts user_input
+
+def coin_amount(cents)
+  # input coin values
+  
+  
 end
